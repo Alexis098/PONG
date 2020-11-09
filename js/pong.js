25d999580c3dbcf9127a8bf4e567e291d36ed8ab
Alexis098 <alhng@free.fr>
2020-11-09 21:38:39 +0100

    raquettes

--- js/pong.js
+++ js/pong.js
@@ -6,7 +6,7 @@
 
 
 
-// Jquerrie perme de faire le lien entre html/css et le fichier js 
+// Jquerry perme de faire le lien entre html/css et le fichier js 
 
 class Terrain{ //différent de la class css
     constructor($html){ //constructor permet d'attribuer des valeurs à ce qu'il y a dans les paranthèses
@@ -14,6 +14,7 @@ class Terrain{ //différent de la class css
         // $element 
         this.largeur=$("#terrain").width(); // fait appel à l'élement css width de l'id terrain du css grâce au $
         this.hauteur=$("#terrain").height(); // on récupère/importe la valeur de la hauteur du terrain depuis le css
+        
     }
 }
 let terrain=new Terrain($("#terrain")); //déclare la variable terrain prenant la valeur de la class Terrain ci-dessus à laquelle on associe les valeurs de la l'id terrain de la fiche css
@@ -36,11 +37,41 @@ class Balle{ //Une classe sert seulement pour y répertorier des variables
 
 let balle = new Balle($("#balle"));
 
+class RaquetteG{ //dans les classes, on ne fait que déclarer des variables, pour faire des actions, il faut utiliser une fonction
+    constructor($html){
+        this.$html=$html;
+        this.haut=parseInt($("#raquetteG").css("top"));
+        this.vitesse=0.5;
+        this.hauteur=parseInt($("#raquetteG").css("height"));
+    }
+    majHTML(){
+        this.$html.css("top",raquetteG.haut);
+    }
+
+}
+
+class RaquetteD{
+    constructor($html){
+        this.$html=$html;
+        this.hauteur=parseInt($("#raquetteD").css("height"));
+        this.haut=parseInt($("#raquetteD").css("top"));
+        this.vitesse=0.5;
+    }
+    majHTML(){
+        this.$html.css("top",raquetteD.haut);
+    }
+}
+
+let raquetteG = new RaquetteG($("#raquetteG"));
+let raquetteD = new RaquetteD($("#raquetteD"));
+
 setInterval(function(){ //cette fonction permet de déplacer la balle 
     balle.gauche=balle.gauche+balle.vitesseX; //la valeur gauche de la variable balle est appelée ici pour prendre la valeur balle.gauche+balle.vitesseX pour déplacer la balle
     balle.haut=balle.haut+balle.vitesseY;
     //on fait appel à la valeur "left" de l'id balle css et on lui donne le nom "gauche" dans le fichier js
     //on applique la variable haut à la div possédant l'id #balle
+    
+
     if(balle.gauche>terrain.largeur){
         balle.gauche=terrain.largeur; 
         balle.vitesseX=balle.vitesseX*-1;
@@ -58,8 +89,31 @@ setInterval(function(){ //cette fonction permet de déplacer la balle
         balle.haut=0;
         balle.vitesseY=balle.vitesseY*-1;
     }
+
+    raquetteG.haut=raquetteG.haut+raquetteG.vitesse;
+    raquetteD.haut=raquetteD.haut+raquetteD.vitesse;
+
+    if(raquetteG.haut>terrain.hauteur-raquetteG.hauteur){
+        raquetteG.haut=terrain.hauteur-raquetteG.hauteur;
+        raquetteG.vitesse=raquetteG.vitesse*-1;
+    }
+    if(raquetteG.haut<0){
+        raquetteG.haut=0;
+        raquetteG.vitesse=raquetteG.vitesse*-1;
+    }
+    if(raquetteD.haut>terrain.hauteur-2*raquetteD.hauteur){
+        raquetteD.haut=terrain.hauteur-2*raquetteD.hauteur;
+        raquetteD.vitesse=raquetteD.vitesse*-1;
+    }
+    if(raquetteD.haut<0-raquetteD.hauteur){
+        raquetteD.haut=0-raquetteD.hauteur;
+        raquetteD.vitesse=raquetteD.vitesse*-1;
+    }
     
     balle.majHTML();
+    raquetteG.majHTML();
+    raquetteD.majHTML();
+
 }, 10);
 
 // on fait des classes et on déclare les variables dedans pour ensuite les utiliser dans les bouces if
