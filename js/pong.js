e8746e868135f569393c0daf6b491f85ecf724bf
Alexis098 <alhng@free.fr>
2020-11-10 10:27:11 +0100

    day2

--- js/pong.js
+++ js/pong.js
@@ -14,11 +14,12 @@ class Terrain{ //différent de la class css
         // $element 
         this.largeur=$("#terrain").width(); // fait appel à l'élement css width de l'id terrain du css grâce au $
         this.hauteur=$("#terrain").height(); // on récupère/importe la valeur de la hauteur du terrain depuis le css
-        
     }
 }
+
+
 let terrain=new Terrain($("#terrain")); //déclare la variable terrain prenant la valeur de la class Terrain ci-dessus à laquelle on associe les valeurs de la l'id terrain de la fiche css
-console.log(terrain);
+console.log(terrain); //affiche les valeurs de la variable terrain dans la console du navigateur web
 
 class Balle{ //Une classe sert seulement pour y répertorier des variables
     constructor($html){
@@ -27,6 +28,8 @@ class Balle{ //Une classe sert seulement pour y répertorier des variables
         this.gauche=parseInt($("#balle").css("left")); // this permet de déclarer une variable dans une classe et let permet de déclarer des variables en dehors des classes
         this.vitesseX=2;
         this.vitesseY=0.5;
+        this.largeur=$('#balle').width(); //on fait appel à la valeur width de l'id balle du css pour l'intégrer dans le fichier js
+        this.hauteur=$("#balle").height();
     }
     majHTML(){ // la fonction "mise à jour html"
         this.$html.css("left",balle.gauche);
@@ -34,7 +37,7 @@ class Balle{ //Une classe sert seulement pour y répertorier des variables
     }
 }
 
-
+// déclaration de la variable js balle qui reprend les valeurs de la classe Balle qui reprend des valeurs css de l'id balle
 let balle = new Balle($("#balle"));
 
 class RaquetteG{ //dans les classes, on ne fait que déclarer des variables, pour faire des actions, il faut utiliser une fonction
@@ -68,43 +71,57 @@ let raquetteD = new RaquetteD($("#raquetteD"));
 setInterval(function(){ //cette fonction permet de déplacer la balle 
     balle.gauche=balle.gauche+balle.vitesseX; //la valeur gauche de la variable balle est appelée ici pour prendre la valeur balle.gauche+balle.vitesseX pour déplacer la balle
     balle.haut=balle.haut+balle.vitesseY;
-    //on fait appel à la valeur "left" de l'id balle css et on lui donne le nom "gauche" dans le fichier js
+    //on fait appel à la valeur "left" de l'id balle css et on lui donne le nom "gauche" dans la variable "balle" dans le fichier js
     //on applique la variable haut à la div possédant l'id #balle
     
-
-    if(balle.gauche>terrain.largeur){
-        balle.gauche=terrain.largeur; 
+    //-balle.largeur pour que la balle ne dépasse pas le bord du terrain car le point de "collision" de la balle se situe en haut à gauche
+    // la balle rebondit lorsqu'elle touche la droite du terrain
+    if(balle.gauche>terrain.largeur-balle.largeur){ 
+        balle.gauche=terrain.largeur-balle.largeur; 
         balle.vitesseX=balle.vitesseX*-1;
     }
-    // si la valeur gauche de la classe balle
+    // la valeur gauche de la classe balle
+    // la balle rebondit lorsqu'elle touche la gauche du terrain
     if(balle.gauche<0){  
         balle.gauche=0;
         balle.vitesseX=balle.vitesseX*-1;
     }
-    if(balle.haut>terrain.hauteur){
-        balle.haut=terrain.hauteur;
+
+    // la balle rebondit lorsqu'elle touche le bas du terrain
+    if(balle.haut>terrain.hauteur-balle.hauteur){
+        balle.haut=terrain.hauteur-balle.hauteur;
         balle.vitesseY=balle.vitesseY*-1;
     }
+
+    // la balle rebondit lorsqu'elle touche le haut du terrain
     if(balle.haut<0){
         balle.haut=0;
         balle.vitesseY=balle.vitesseY*-1;
     }
 
+    // on applique gauche la vitesse de déplacement pour les deux raquettes
     raquetteG.haut=raquetteG.haut+raquetteG.vitesse;
     raquetteD.haut=raquetteD.haut+raquetteD.vitesse;
 
+    // la raquette rebondit lorsqu'elle touche le bas du terrain
     if(raquetteG.haut>terrain.hauteur-raquetteG.hauteur){
         raquetteG.haut=terrain.hauteur-raquetteG.hauteur;
         raquetteG.vitesse=raquetteG.vitesse*-1;
     }
+
+    // la raquette gauche rebondit lorsqu'elle touche le haut du terrain
     if(raquetteG.haut<0){
         raquetteG.haut=0;
         raquetteG.vitesse=raquetteG.vitesse*-1;
     }
+
+    // la raquette droite rebondit lorsqu'elle touche le bas du terrain
     if(raquetteD.haut>terrain.hauteur-2*raquetteD.hauteur){
         raquetteD.haut=terrain.hauteur-2*raquetteD.hauteur;
         raquetteD.vitesse=raquetteD.vitesse*-1;
     }
+
+    // la raquette droite rebondit lorsqu'elle touche le haut du terrain
     if(raquetteD.haut<0-raquetteD.hauteur){
         raquetteD.haut=0-raquetteD.hauteur;
         raquetteD.vitesse=raquetteD.vitesse*-1;
